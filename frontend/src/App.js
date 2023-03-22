8628342ecb76dbfcfaaa00c4397023bd9a2860cb
SHEA <nacho.dieguez18@gmail.com>
2023-03-22 19:38:43 -0300

    added frontend navbar and header

---
+++ frontend/src/App.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import './App.css';
+import Home from './pages/Home';
+import NotFound from './pages/NotFound';
+import { BrowserRouter, Route, Routes} from 'react-router-dom';
+import { ThemeProvider } from '@mui/material/styles';
+import CssBaseline from '@mui/material/CssBaseline';
+import { theme } from './theme';
+
+
+const App = () => {
+  return (
+    <>
+      <ThemeProvider theme={theme}>
+        <CssBaseline/>
+        <BrowserRouter>
+				  <Routes>
+					  <Route path='/' element={<Home/>}/>
+		  			<Route path='*' element={<NotFound/>}/>
+			  	</Routes>
+			  </BrowserRouter>
+      </ThemeProvider>
+    </>
+  )
+}
+
+export default App
+
+
+
